26a8040926965f670e88b5cf907ee2da2f3afcf4
Welbert Fernandes <welbert.jsj@gmail.com>
2021-01-14 11:37:24 -0300

    Testando classe Dominto Categoria

--- src/main/java/com/curso/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/curso/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.curso.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 @RequestMapping(method = RequestMethod.GET)	
-	public String listar() {
-		return "Rest esta funcionando!";
+	public List<Categoria> listar() {
+	
+	Categoria cat = new Categoria(1, "Informatica");
+	Categoria cat2 = new Categoria(2, "Escritorio");
+	
+	List<Categoria> categorias = new ArrayList<>();
+	categorias.add(cat);
+	categorias.add(cat2);
+	
+		return categorias;
 	}
 
 }
